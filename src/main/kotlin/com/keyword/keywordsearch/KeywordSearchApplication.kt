f6802c8cb97ad1358e497961197148e9237805dd
sjparkk <sjparkk0820@naver.com>
2023-03-21 22:16:31 +0900

    git init

---
+++ src/main/kotlin/com/keyword/keywordsearch/KeywordSearchApplication.kt
@@ -0,0 +1,11 @@
+package com.keyword.keywordsearch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KeywordSearchApplication
+
+fun main(args: Array<String>) {
+    runApplication<KeywordSearchApplication>(*args)
+}
---
+++ src/test/kotlin/com/keyword/keywordsearch/KeywordSearchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.keyword.keywordsearch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KeywordSearchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
