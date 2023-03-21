1fc95140389b20abfba3f8f5804eddd0149ed7a4
sjparkk <sjparkk0820@naver.com>
2023-03-22 01:26:04 +0900

    feat : 카카오 블로그 검색 API exception 발생 시 retry 로직 추가

--- src/main/kotlin/com/keyword/keywordsearch/service/requester/custom/KakaoBlogSearchRequester.kt
+++ src/main/kotlin/com/keyword/keywordsearch/service/requester/custom/KakaoBlogSearchRequester.kt
@@ -1,10 +1,12 @@
 package com.keyword.keywordsearch.service.requester.custom
 
 import com.keyword.keywordsearch.common.client.channel.kakao.KakaoSearchApiClient
+import com.keyword.keywordsearch.common.client.channel.kakao.dto.ResKakaoBlogCondition
 import com.keyword.keywordsearch.common.entity.EntityEnum
 import com.keyword.keywordsearch.service.dto.BlogKeywordsDTO
 import com.keyword.keywordsearch.service.dto.ResBlogKeywordsDTO
 import com.keyword.keywordsearch.service.requester.BlogSearchRequester
+import org.slf4j.LoggerFactory
 import org.springframework.core.env.Environment
 import org.springframework.data.domain.Pageable
 import org.springframework.stereotype.Component
@@ -16,9 +18,13 @@ class KakaoBlogSearchRequester(
     private val environment: Environment
 ) : BlogSearchRequester {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     //kakao api key & url
     private val kakaoRestApiKey = environment.getProperty("feign.target-api.kakao-blog-search.rest-api-key")!!
     private val kakaoBaseUrl = environment.getProperty("feign.target-api.kakao-blog-search.base-url")!!
+
+    private val maxRetryCount = 3
 
     /**
      * 카카오 블로그 검색 API 호출 및 응답값 공통 작업
@@ -27,9 +33,7 @@ class KakaoBlogSearchRequester(
 
         val sort = pageable.sort.toString().split(":")[0]
 
-        val kakaoResponse = kakaoSearchApiClient.getSearchBlogsByCondition(
-            url = URI(kakaoBaseUrl), auth = kakaoRestApiKey, query = keyword, sort = sort, page = pageable.pageNumber, size = pageable.pageSize
-        )
+        val kakaoResponse = kakaoSearchBlogs(keyword, sort, pageable)
 
         return ResBlogKeywordsDTO(
             list = kakaoResponse.documents.map {
@@ -47,5 +51,31 @@ class KakaoBlogSearchRequester(
             pageableCount = kakaoResponse.meta.pageableCount,
             channelName = EntityEnum.SearchChannelType.KAKAO.value
         )
+    }
+
+    private fun kakaoSearchBlogs(
+        keyword: String,
+        sort: String,
+        pageable: Pageable
+    ): ResKakaoBlogCondition {
+        lateinit var kakaoResponse: ResKakaoBlogCondition
+
+        for (i in 0..maxRetryCount) {
+            try {
+                kakaoResponse = kakaoSearchApiClient.getSearchBlogsByCondition(
+                    url = URI(kakaoBaseUrl),
+                    auth = kakaoRestApiKey,
+                    query = keyword,
+                    sort = sort,
+                    page = pageable.pageNumber,
+                    size = pageable.pageSize
+                )
+                log.debug(":: 카카오 블로그 검색 API 정상 응답")
+            } catch (e: Exception) {
+                log.debug(":: 카카오 채널 $i 번째 예외 발생")
+                if (i == maxRetryCount) throw e
+            }
+        }
+        return kakaoResponse
     }
 }
