3e6119ff09eeac05c917537887286b6fe867e041
sjparkk <sjparkk0820@naver.com>
2023-03-22 00:53:50 +0900

    feat : 페인클라이언트 로그 관련 컨피그 및 스웨거 컨피그 작성

--- src/main/kotlin/com/keyword/keywordsearch/KeywordSearchApplication.kt
+++ src/main/kotlin/com/keyword/keywordsearch/KeywordSearchApplication.kt
@@ -2,8 +2,10 @@ package com.keyword.keywordsearch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
+@EnableFeignClients
 @EnableJpaAuditing
 @SpringBootApplication
 class KeywordSearchApplication
---
+++ src/main/kotlin/com/keyword/keywordsearch/common/config/swagger/SwaggerConfig.kt
@@ -0,0 +1,38 @@
+package com.keyword.keywordsearch.common.config.swagger
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition
+import io.swagger.v3.oas.annotations.servers.Server
+import io.swagger.v3.oas.models.Components
+import io.swagger.v3.oas.models.OpenAPI
+import io.swagger.v3.oas.models.info.Contact
+import io.swagger.v3.oas.models.info.Info
+import org.springdoc.core.GroupedOpenApi
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@OpenAPIDefinition(servers = [Server(url = "/", description = "Default Server URL")])
+@Configuration
+class SwaggerConfig {
+
+    @Bean
+    fun v1API(): GroupedOpenApi {
+        return GroupedOpenApi.builder()
+            .group("blog")
+            .pathsToMatch("/api/blog/**")
+            .build()
+    }
+
+    @Bean
+    fun customOpenAPI(): OpenAPI {
+        val contact = Contact()
+        contact.url = "http://localhost:8080/swagger-ui/index.html"
+        return OpenAPI()
+            .components(Components())
+            .info(
+                Info().title("Blog Search Service HTTP API")
+                    .description("API 명세 문서는 아래 URL 클릭")
+                    .contact(contact)
+                    .version("1.0.0")
+            )
+    }
+}
