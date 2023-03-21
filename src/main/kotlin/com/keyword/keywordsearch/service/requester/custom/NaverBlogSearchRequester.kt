5095aca4d638fd73dd2af18aed4195f228d452ac
sjparkk <sjparkk0820@naver.com>
2023-03-22 01:26:49 +0900

    feat : 네이버 블로그 검색 API exception 발생 시 retry 로직 추가 및 exception case 추가

--- src/main/kotlin/com/keyword/keywordsearch/service/requester/custom/NaverBlogSearchRequester.kt
+++ src/main/kotlin/com/keyword/keywordsearch/service/requester/custom/NaverBlogSearchRequester.kt
@@ -2,12 +2,19 @@ package com.keyword.keywordsearch.service.requester.custom
 
 import com.keyword.keywordsearch.service.requester.BlogSearchRequester
 import com.keyword.keywordsearch.common.client.channel.naver.NaverSearchApiClient
+import com.keyword.keywordsearch.common.client.channel.naver.dto.ResNaverBlogCondition
 import com.keyword.keywordsearch.common.entity.EntityEnum
+import com.keyword.keywordsearch.common.error.CustomBadRequestException
+import com.keyword.keywordsearch.common.error.CustomInternalServerException
+import com.keyword.keywordsearch.common.error.CustomUnauthorizedException
 import com.keyword.keywordsearch.service.dto.BlogKeywordsDTO
 import com.keyword.keywordsearch.service.dto.ResBlogKeywordsDTO
+import org.slf4j.LoggerFactory
 import org.springframework.core.env.Environment
 import org.springframework.data.domain.Pageable
+import org.springframework.http.HttpStatus
 import org.springframework.stereotype.Component
+import java.net.HttpURLConnection
 import java.net.URI
 
 @Component
@@ -16,20 +23,21 @@ class NaverBlogSearchRequester(
     private val environment: Environment
 ) : BlogSearchRequester {
 
+    private val log = LoggerFactory.getLogger(javaClass)
+
     //naver api key & url
     private val naverClientId = environment.getProperty("feign.target-api.naver-blog-search.client-id")!!
     private val naverClientSecret = environment.getProperty("feign.target-api.naver-blog-search.client-secret")!!
     private val naverBaseUrl = environment.getProperty("feign.target-api.naver-blog-search.base-url")!!
+
+    private val maxRetryCount = 3
 
     /**
      * 네이버 블로그 검색 API 호출 및 응답값 공통 작업
      */
     override fun searchBlogs(keyword: String, pageable: Pageable): ResBlogKeywordsDTO {
 
-        val naverResponse = naverSearchApiClient.getSearchBlogsByCondition(
-            url = URI(naverBaseUrl), clientId = naverClientId, clientSecret = naverClientSecret, query = keyword, display = pageable.pageSize, start = pageable.pageNumber
-        )
-
+        val naverResponse: ResNaverBlogCondition = naverSearchBlogs(keyword, pageable)
         return ResBlogKeywordsDTO(
             list = naverResponse.items.map {
                 BlogKeywordsDTO(
@@ -43,6 +51,41 @@ class NaverBlogSearchRequester(
             totalCount = naverResponse.total,
             channelName = EntityEnum.SearchChannelType.NAVER.value
         )
+    }
+
+    private fun naverSearchBlogs(
+        keyword: String,
+        pageable: Pageable
+    ): ResNaverBlogCondition {
+        lateinit var naverResponse: ResNaverBlogCondition
+
+        try {
+            for (i in 0..maxRetryCount) {
+                try {
+                    naverResponse = naverSearchApiClient.getSearchBlogsByCondition(
+                        url = URI(naverBaseUrl),
+                        clientId = naverClientId,
+                        clientSecret = naverClientSecret,
+                        query = keyword,
+                        display = pageable.pageSize,
+                        start = pageable.pageNumber
+                    )
+                    log.debug(":: 네이버 블로그 검색 API 정상 응답")
+                } catch (e: Exception) {
+                    log.debug(":: 네이버 채널 $i 번째 예외 발생")
+                    if (i == maxRetryCount) throw e
+                }
+            }
+        } catch (e: Exception) {
+            //Exception Handler 를 통한 정의 된 Exception 처리
+            when((e as HttpURLConnection).responseCode) {
+                HttpStatus.BAD_REQUEST.value() -> throw CustomBadRequestException(e.message)
+                HttpStatus.UNAUTHORIZED.value() -> throw CustomUnauthorizedException(e.message)
+                HttpStatus.INTERNAL_SERVER_ERROR.value() -> throw CustomInternalServerException(e.message)
+            }
+        }
+
+        return naverResponse
     }
 
 }
