81995f728a184e95abef20709322aed432ca6aaa
sjparkk <sjparkk0820@naver.com>
2023-03-22 20:52:11 +0900

    feat : 캐시 및 스케줄러 Enable 어노테이션 추가

--- src/main/kotlin/com/keyword/keywordsearch/KeywordSearchApplication.kt
+++ src/main/kotlin/com/keyword/keywordsearch/KeywordSearchApplication.kt
@@ -2,11 +2,15 @@ package com.keyword.keywordsearch
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cache.annotation.EnableCaching
 import org.springframework.cloud.openfeign.EnableFeignClients
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @EnableFeignClients
 @EnableJpaAuditing
+@EnableCaching
+@EnableScheduling
 @SpringBootApplication
 class KeywordSearchApplication
 
