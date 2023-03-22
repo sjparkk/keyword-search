2d43af4fd92449300848cb492a9b40b2f21fe888
sjparkk <sjparkk0820@naver.com>
2023-03-22 23:39:34 +0900

    fix - 스웨거 어노테이션 수정

--- src/main/kotlin/com/keyword/keywordsearch/controller/swagger/PopularKeywordApiResponse.kt
+++ src/main/kotlin/com/keyword/keywordsearch/controller/swagger/PopularKeywordApiResponse.kt
@@ -1,8 +1,13 @@
 package com.keyword.keywordsearch.controller.swagger
 
+import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 
+@Operation(summary = "인기 검색어 목록 조회", description = """
+        - 사용자들이 많이 검색한 순서대로 최대 10개의 검색 키워드를 제공합니다.
+        - 검색어 별로 검색 된 횟수도 함께 제공합니다.
+    """)
 @ApiResponses(
     ApiResponse(
         responseCode = "204",
